42568f4ea8eca51158ec488a039600b40bb52a77
ypablo <yfedora@gmail.com>
2020-01-31 22:47:27 +0000

    Add users

--- src/components/DayNames/DayNames.js
+++ src/components/DayNames/DayNames.js
@@ -3,33 +3,16 @@ import "./DayNames.css"
 
 export default function Cells() {
 
-    //const month = new Date().getMonth() + 1
-    //const year = new Date().getFullYear()
-    //const numberDaysOfMonth  = new Date(year, month, 0).getDate();
 
-    //let d = new Date()
-    //const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
-
-    //var date = new Date(dateStr);
-    //date.toLocaleDateString(locale, { weekday: 'long' }); 
-
-    let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
-
-
-
-    // let foo = []
-    // for (let i = 1; i <= numberDaysOfMonth; i++) {
-    //     foo.push(i)
-    // }
-
-    // let nameOfTheDay = weekday.map(elem => {
-    //     return <div className="structure" key={elem}>
-    //         {elem}</div> 
-    // })
 
     return (
         <div className="structure">
-            {weekday}
+            <div className="operator">Charlotte</div>
+            <div className="operator">Clint</div>
+            <div className="operator">Michael</div>
+            <div className="operator">Pawel</div>
+            <div className="operator">Phil</div>
+            <div className="operator">Simon</div>
         </div>
     )
 }
